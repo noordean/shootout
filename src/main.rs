ad436c596358b5c096c3957724cd4afec7abe6bd
noordean <ebroyeem90@gmail.com>
2019-11-14 07:24:06 +0100

    Create game template

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use ggez::*;
+
+struct State {
+  dt: std::time::Duration
+}
+
+impl ggez::event::EventHandler for State {
+  fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
+    self.dt = timer::delta(ctx);
+    Ok(())
+  }
+
+  fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+    println!("Hello ggez! dt = {}ns", self.dt.subsec_nanos());
+    Ok(())
+  }
+}
+
+fn main() {
+  let state = &mut State {
+    dt: std::time::Duration::new(0, 0)
+  };
+
+  let c = conf::Conf::new();
+  let (ref mut ctx, ref mut event_loop) = ContextBuilder::new("hello_ggez", "awesome_person")
+      .conf(c)
+      .build()
+      .unwrap();
+
+  event::run(ctx, event_loop, state).unwrap();
+}
